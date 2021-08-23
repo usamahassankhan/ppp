ad4d7c32a579e3732ebe8d96c347e02c856020ce
usamahassankhan <intern.dev6.cc@gmail.com>
2021-08-23 15:38:46 +0000

    dsda

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './home/home'
 function App() {
   return (
+    <>
    <div className="mai">
      this is this
    </div>
-      
+   <Home/>
+      </>
   );
 }
 
---
+++ src/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css'
+
+const home = () => {
+    return (
+        <div  className="wao">
+            wao its nice
+        </div>
+    )
+}
+
+export default home
